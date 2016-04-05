5464745fc923d3c8672413a6939ccfbc7c8411a8
Rob Watling <robmwatling@gmail.com>
2016-04-05 09:53:44 +0100

    Raycast Selection - Example Working Allows you to click into the scene and interact with an object.

---
+++ RaycastSelect/Assets/Scripts/ExampleRaycast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleRaycast : MonoBehaviour
+{
+	private void Update ()
+    {
+	    if (Input.GetButtonDown("Fire1"))
+	    {
+	        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit))
+	        {
+	            if (raycastHit.transform.GetComponent<Renderer>().material.color == Color.red)
+	            {
+                    raycastHit.transform.GetComponent<Renderer>().material.color = Color.white;
+                }
+	            else
+	            {
+                    raycastHit.transform.GetComponent<Renderer>().material.color = Color.red;
+                }
+	        }               
+	    }
+    }
+}
