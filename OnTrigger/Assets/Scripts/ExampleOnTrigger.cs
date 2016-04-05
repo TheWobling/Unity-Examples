f58bdeba42895d1ac785c8488cdeb166e2d17a95
Rob Watling <robmwatling@gmail.com>
2016-04-05 09:36:14 +0100

    OnTriggerEnter & Exit - Working Example.

---
+++ OnTrigger/Assets/Scripts/ExampleOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleOnTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(transform.name + " has entered a trigger with " + collider.transform.name);
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        Debug.Log(transform.name + " has exited a trigger with " + collider.transform.name);
+    }
+}
