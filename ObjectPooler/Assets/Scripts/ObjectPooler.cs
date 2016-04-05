7db8acb296b570676b5f6d0cb04652f3075ec57a
Rob Watling <robmwatling@gmail.com>
2016-04-05 12:28:48 +0100

    Generic Object Pooler - Example Working.

---
+++ ObjectPooler/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler CurrentObjectPooler;
+
+    [SerializeField] private int _startPoolCount;
+    [SerializeField] private int _maxPoolCount;
+    [SerializeField] private bool _canGrow;
+
+    [SerializeField] private GameObject _pooledObject;
+    [SerializeField] private List<GameObject> _pooledObjects;
+
+    private void Awake()
+    {
+        CurrentObjectPooler = this;
+    }
+
+	private void Start ()
+    {
+        for (var i = 0; i < _startPoolCount; i++)
+        {
+            GameObject obj = Instantiate(_pooledObject);
+            obj.SetActive(false);
+            _pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        for (var i = 0; i < _pooledObjects.Count; i++)
+        {
+            if (!_pooledObjects[i].activeInHierarchy)
+            {
+                return _pooledObjects[i];
+            }
+        }
+
+        if(_canGrow && _pooledObjects.Count < _maxPoolCount)
+        {
+            GameObject obj = Instantiate(_pooledObject);
+            _pooledObjects.Add(obj);
+            return obj;
+        }
+
+        return null;
+    }
+}
---
+++ ObjectPooler/Assets/Scripts/RequestPooledObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RequestPooledObject : MonoBehaviour
+{
+    private float _distance = 10.0f;
+
+	void Update ()
+    {
+	    if (Input.GetButtonDown("Fire1"))
+	    {
+	        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _distance));
+	        GameObject obj = ObjectPooler.CurrentObjectPooler.GetObject();
+
+	        if (obj == null) return;
+
+	        obj.transform.position = targetPosition;
+            obj.SetActive(true);
+	    }
+	}
+}
