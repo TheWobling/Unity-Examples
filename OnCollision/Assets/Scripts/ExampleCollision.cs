84bf724275ade56bef547da5336b90a7c63f4f63
Rob Watling <robmwatling@gmail.com>
2016-04-05 09:23:57 +0100

    OnCollisionEnter - Example works.

---
+++ OnCollision/Assets/Scripts/ExampleCollision.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(transform.name + " has collided with " + collision.transform.name);
+    }
+}
